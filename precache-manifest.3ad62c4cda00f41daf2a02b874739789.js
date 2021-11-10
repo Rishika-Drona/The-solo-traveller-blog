2987046c0bb19449ec76e58380a8e6496fcc2f7e
Rishika Drona <74383234+Rishika-Drona@users.noreply.github.com>
2021-11-09 17:58:25 -0800

    Add files via upload

---
+++ precache-manifest.3ad62c4cda00f41daf2a02b874739789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62d3613b2d8d78b3cf6b86c1e112425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e86df2b62072c1262b",
+    "url": "/static/css/main.d574169e.chunk.css"
+  },
+  {
+    "revision": "30ea45fa44b98c36e6e2",
+    "url": "/static/js/2.790c268f.chunk.js"
+  },
+  {
+    "revision": "54e86df2b62072c1262b",
+    "url": "/static/js/main.2e6e2820.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "d88f8fbbd802468d9bb175d2259a2890",
+    "url": "/static/media/BlogLanding.d88f8fbb.jpeg"
+  }
+]);
